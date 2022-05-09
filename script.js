90a41f62d41dc0a75987bfdda10b6759777f73fe
Himanshu Gupta <25himanshugupta@gmail.com>
2022-05-10 03:02:52 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,22 @@
+if ('OTPCredential' in window) {
+    window.addEventListener('DOMContentLoaded', e => {
+      const input = document.querySelector('input[autocomplete="one-time-code"]');
+      if (!input) return;
+      const ac = new AbortController();
+      const form = input.closest('form');
+      if (form) {
+        form.addEventListener('submit', e => {
+          ac.abort();
+        });
+      }
+      navigator.credentials.get({
+        otp: { transport:['sms'] },
+        signal: ac.signal
+      }).then(otp => {
+        input.value = otp.code;
+        if (form) form.submit();
+      }).catch(err => {
+        console.log(err);
+      });
+    });
+  }
